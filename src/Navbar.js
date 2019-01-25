6f72cdf52edb5dd0cd50b13b4bec7aba1d81f3bf
Your Name <jacob.baordman.jb@gmail.com>
2019-01-25 08:54:43 +0000

    formatted

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 //import './Navbar.css';
 import Homepage from './Homepage.js';
 import Users from './Users.js';
@@ -9,20 +9,16 @@ class Navbar extends Component {
     render() {
         return (
             <Router>
-            <div className="navBar">
-                <div>
+                <div className="navBar">
                     <ul>
                         <li className="navItem"><h3><NavLink exact={true} activeClassName='is-active' to="/">Home</NavLink></h3></li>
-                        <li className="navItem"><h3><NavLink activeClassName='is-active'  to="/users">Users</NavLink></h3></li>
-                        <li className="navItem"><h3><NavLink activeClassName='is-active'  to="/games">Games</NavLink></h3></li>
+                        <li className="navItem"><h3><NavLink activeClassName='is-active' to="/users">Users</NavLink></h3></li>
+                        <li className="navItem"><h3><NavLink activeClassName='is-active' to="/games">Games</NavLink></h3></li>
                     </ul>
-
-                <Route exact path="/" component={Homepage}/>
-                <Route path="/users" component={Users} />
-                <Route path="/games" component={Games} />
-
+                    <Route exact path="/" component={Homepage} />
+                    <Route path="/users" component={Users} />
+                    <Route path="/games" component={Games} />
                 </div>
-            </div>
             </Router>
         );
     }
