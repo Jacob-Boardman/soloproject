39ab274b0cc2946ff20c1f48e6f987e9737258a5
Your Name <jacob.baordman.jb@gmail.com>
2019-02-01 09:14:35 +0000

    added in blank matcher page

---
+++ src/MatcherDOM.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MatcherDOM extends Component {
+    render() {
+        return(
+            <div className="gameMatcher">
+                Hello Message
+            </div>
+        )
+    }
+}
+
+export default MatcherDOM;
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Homepage from './Homepage.js';
 import UsersDOM from './User/UsersDOM.js';
 import GamesDOM from './Game/GamesDOM.js';
+import MatcherDOM from './MatcherDOM.js';
 import './Stylesheets/Navbar.css';
 
 class Navbar extends Component {
@@ -14,10 +15,12 @@ class Navbar extends Component {
                         <li className="navItem"><h3><NavLink exact={true} activeClassName='is-active' to="/">Home</NavLink></h3></li>
                         <li className="navItem"><h3><NavLink activeClassName='is-active' to="/users">Users</NavLink></h3></li>
                         <li className="navItem"><h3><NavLink activeClassName='is-active' to="/games">Games</NavLink></h3></li>
+                        <li className="navItem"><h3><NavLink activeClassName='is-active' to="/matcher">Matcher</NavLink></h3></li>
                     </ul>
                     <Route exact path="/" component={Homepage} />
                     <Route path="/users" component={UsersDOM} />
                     <Route path="/games" component={GamesDOM} />
+                    <Route path="/matcher" component={MatcherDOM} />
                 </div>
             </Router>
         );
